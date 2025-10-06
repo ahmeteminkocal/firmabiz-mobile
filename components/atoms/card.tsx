300927e6544018824db408b26925a9e125c696ea
Kais Jarbou <kais.jarbou@evolvextechnology.com>
2025-10-06 16:49:11 +0400

    Card is pressable only when needed

--- components/atoms/card.tsx
+++ components/atoms/card.tsx
@@ -1,21 +1,28 @@
-import { cn } from '@/lib/utils'
-import React from 'react'
-import { Pressable, PressableProps } from 'react-native'
+import { cn } from '@/lib/utils';
+import React from 'react';
+import { GestureResponderEvent, Pressable, View, ViewProps } from 'react-native';
 
 export function Card({
   className,
   ...props
-}: PressableProps){
+}: ViewProps & {
+    onPress?: ((event: GestureResponderEvent) => void) | null | undefined
+}){
+    const cardClass = cn('bg-background rounded-[5px] border border-border p-3', className);
+    if(props.onPress){
+        return (
+            <Pressable 
+                className={cardClass}
+                {...props}
+            />
+        );
+    }
     return (
-        <Pressable 
-            pointerEvents={props.onPress? 'auto' : 'box-none'}
-            className={cn(
-                'bg-background rounded-[5px] border border-border p-3',
-                className
-            )}
+        <View 
+            className={cardClass}
             {...props}
         />
-    )
+    );
 }
 
 export default Card
